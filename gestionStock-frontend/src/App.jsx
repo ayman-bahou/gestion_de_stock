662468f038c2795ce94e5a7f9b83b32babbaf331
ayman-bahou <ayman.bahou@etu.toulouse-inp.fr>
2025-02-07 23:25:29 +0100

    debut frontend

---
+++ gestionStock-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <section className="bg-blue-900 h-screen">
+      <div className="container">
+        <form
+          action=""
+          className="text-2xl text-center flex flex-col justify-center items-center h-full "
+        >
+          <div className="form-group">
+            
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default App;
